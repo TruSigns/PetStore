cfb172ee73a06ce14f96c9b059d8a84619c90494
Maurice Ruffin <Tysons3x3@gmail.com>
2021-06-22 15:17:33 -0400

    petstore

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import ListOfItems from "./component/ListOfitems";
+import PetStore from "./component/PetStore";
+
+function App() {
+  return (
+    <div className="App">
+      {/**<ListOfItems /> */}
+      <PetStore />
+    </div>
+  );
+}
+
+export default App;
